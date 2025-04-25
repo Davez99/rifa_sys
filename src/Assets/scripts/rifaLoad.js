4dcbb541aff84c1d3503f838b0aafc6be24e21e0
Davez <69636802+Davez99@users.noreply.github.com>
2025-04-25 14:00:54 -0300

    Update rifaLoad.js

--- src/Assets/scripts/rifaLoad.js
+++ src/Assets/scripts/rifaLoad.js
@@ -38,7 +38,7 @@ function renderizarRifa(data) {
   document.getElementById("valor").textContent = ` R$ ${parseFloat(data.valor).toFixed(2)}`;
   document.getElementById("premiacao").textContent = data.premiacao;
 
-  valorRifa = data.valor.toFixed(2);
+  valorRifa = ` R$ ${parseFloat(data.valor).toFixed(2)}`;
 
   const container = document.getElementById("numerosContainer");
   container.innerHTML = "";
